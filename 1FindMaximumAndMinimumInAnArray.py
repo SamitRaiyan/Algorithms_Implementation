76f7a632c7c0c80850ec6849cdb7790bc359bb39
SamitRaiyan <samitr247@gmail.com>
2025-03-29 13:34:26 +0600

    1 no algorithm solved

--- 1FindMaximumAndMinimumInAnArray.py
+++ 1FindMaximumAndMinimumInAnArray.py
@@ -1,1 +1,20 @@
 # Algorithm-1: Find Maximum & Minimum in an Array
+
+def find_max_min(arr):
+    max_val = min_val = arr[0]
+
+    # Iterate through the array
+    for num in arr:
+        if num > max_val:
+            max_val = num
+        if num < min_val:
+            min_val = num
+
+    return max_val, min_val
+
+# Taking dynamic input
+arr = list(map(int, input("Enter the array elements separated by space: ").split()))
+
+max_value, min_value = find_max_min(arr)
+print("Max:", max_value)
+print("Min:", min_value)
