5867685513e2c8797eef562d8ef078394af92dbe
SamitRaiyan <samitr247@gmail.com>
2025-04-01 10:48:38 +0600

    Algorithm 2 is solved

--- 2.CheckIfANumberisEvenorOdd.py
+++ 2.CheckIfANumberisEvenorOdd.py
@@ -1,1 +1,12 @@
 # Algorithm-2: Check if a Number is Even or Odd
+
+number = int(input('Enter a number: '))
+
+if number == 0:
+    print('Number is Zero which is a confusing number.')
+
+elif number%2 == 0:
+    print('Number is Even')
+
+else:
+    print('Number is Odd')
