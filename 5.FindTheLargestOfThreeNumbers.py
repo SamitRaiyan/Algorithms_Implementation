b110670a4d42ccfb8ee7c6544991fcb0acd2347c
SamitRaiyan <samitr247@gmail.com>
2025-04-02 00:48:09 +0600

    5th algorithm solved

--- 5.FindTheLargestOfThreeNumbers.py
+++ 5.FindTheLargestOfThreeNumbers.py
@@ -1,1 +1,12 @@
 # 5. Find the Largest of Three Numbers
+
+a, b, c = map(int, input("Enter three numbers: ").split())
+
+if a>b and a>c:
+    print(f"{a} is the largest number.")
+
+elif b>a and b>c:
+    print(f"{b} is the largest number.")
+
+else:
+    print(f"{c} is the largest number.")
