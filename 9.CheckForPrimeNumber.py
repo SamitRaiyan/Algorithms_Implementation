e592095351a77b11ebbfd949796841bb6d5aac6f
SamitRaiyan <samitr247@gmail.com>
2025-04-03 01:03:48 +0600

    algorithm 9 is solved

---
+++ 9.CheckForPrimeNumber.py
@@ -0,0 +1,12 @@
+# 9. Check for Prime Number
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+num = int(input("Enter a number: "))
+print("Prime" if is_prime(num) else "Not Prime")
